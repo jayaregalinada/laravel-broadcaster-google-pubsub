fbc722597b6609caca553870d20746822b0390e1
Jay Are Galinada <jayaregalinada@gmail.com>
2020-05-05 20:05:42 +0800

    ⚠️ Breaking changes - Renamed projectId to project_id for consistency and readability - Renamed keyFilePath to credentials_path

--- config/google.php
+++ config/google.php
@@ -2,8 +2,8 @@
 
 return [
     'driver' => 'google',
-    'projectId' => env('GOOGLE_PUBSUB_BROADCASTER_PROJECT_ID', env('GOOGLE_PROJECT_ID', env('GCLOUD_PROJECT'))),
-    'keyFilePath' => env('GOOGLE_PUBSUB_BROADCASTER_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
+    'project_id' => env('GOOGLE_PUBSUB_BROADCASTER_PROJECT_ID', env('GOOGLE_PROJECT_ID', env('GCLOUD_PROJECT'))),
+    'credentials_path' => env('GOOGLE_PUBSUB_BROADCASTER_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
     'auto_create_topic' => env('GOOGLE_PUBSUB_BROADCASTER_AUTO_CREATE_TOPIC', false),
     'payload_class' => null,
     'override_config' => [],
