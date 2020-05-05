9fb9049333e656a30c8b8831d65ee8419b636835
Jay Are Galinada <jayaregalinada@gmail.com>
2020-05-05 12:31:39 +0800

    Rename default environment key with new configurations - Added `auto_create_topic` to create topic if does not exists or throw an error - Added `payload_class` to customize payload - Added `override_config` to override configuration - Renamed environment with additional `*_BROADCASTER_*` to distinguished configuration

--- config/google.php
+++ config/google.php
@@ -2,6 +2,9 @@
 
 return [
     'driver' => 'google',
-    'projectId' => env('GOOGLE_PUBSUB_PROJECT_ID', env('GOOGLE_PROJECT_ID', env('GCLOUD_PROJECT'))),
-    'keyFilePath' => env('GOOGLE_PUBSUB_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS'))
+    'projectId' => env('GOOGLE_PUBSUB_BROADCASTER_PROJECT_ID', env('GOOGLE_PROJECT_ID', env('GCLOUD_PROJECT'))),
+    'keyFilePath' => env('GOOGLE_PUBSUB_BROADCASTER_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
+    'auto_create_topic' => env('GOOGLE_PUBSUB_BROADCASTER_AUTO_CREATE_TOPIC', false),
+    'payload_class' => null,
+    'override_config' => [],
 ];
