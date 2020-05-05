9b0d362660136df8c7e6e68f7b174136d26b8dc7
Jay Are Galinada <jayaregalinada@gmail.com>
2020-05-05 12:33:30 +0800

    Added additional properties - Reformat codes - Used inline class - Include topic in payload

--- src/Broadcasters/GooglePubSubBroadcaster.php
+++ src/Broadcasters/GooglePubSubBroadcaster.php
@@ -3,12 +3,15 @@
 namespace Jag\Broadcaster\GooglePubSub\Broadcasters;
 
 use Psr\Log\LoggerInterface;
+use Google\Cloud\PubSub\Topic;
 use Google\Cloud\PubSub\Message;
 use Illuminate\Support\Collection;
 use Illuminate\Broadcasting\Channel;
 use Google\Cloud\PubSub\PubSubClient;
+use Jag\Contracts\GooglePubSub\Payload;
+use Google\Cloud\Core\Exception\NotFoundException;
 use Illuminate\Broadcasting\Broadcasters\Broadcaster;
-use Jag\Broadcaster\GooglePubSub\Contract\PubSubClientContract;
+use Jag\Exceptions\GooglePubSub\TopicNotFoundException;
 
 class GooglePubSubBroadcaster extends Broadcaster
 {
@@ -23,21 +26,39 @@ class GooglePubSubBroadcaster extends Broadcaster
     protected $logger;
 
     /**
+     * @var bool
+     */
+    protected $autoCreateTopic;
+
+    /**
+     * @var \Jag\Contracts\GooglePubSub\Payload
+     */
+    protected $payloadClass;
+
+    /**
      * GooglePubSubBroadcaster constructor.
      *
-     * @param \Google\Cloud\PubSub\PubSubClient|PubSubClientContract $client
-     * @param \Psr\Log\LoggerInterface          $logger
+     * @param \Google\Cloud\PubSub\PubSubClient|\Jag\Contracts\GooglePubSub\PubSubClient $client
+     * @param \Psr\Log\LoggerInterface                                                   $logger
+     * @param bool                                                                       $autoCreateTopic
+     * @param \Jag\Contracts\GooglePubSub\Payload|null                                   $payloadClass
      */
-    public function __construct(PubSubClient $client, LoggerInterface $logger)
-    {
+    public function __construct(
+        PubSubClient $client,
+        LoggerInterface $logger,
+        $autoCreateTopic = false,
+        Payload $payloadClass = null
+    ) {
         $this->client = $client;
         $this->logger = $logger;
+        $this->autoCreateTopic = $autoCreateTopic;
+        $this->payloadClass = $payloadClass;
     }
 
     /**
-     * @return \Google\Cloud\PubSub\PubSubClient|PubSubClientContract
+     * @return \Google\Cloud\PubSub\PubSubClient|\Jag\Contracts\GooglePubSub\PubSubClient
      */
-    public function getClient() : PubSubClientContract
+    public function getClient()
     {
         return $this->client;
     }
@@ -77,38 +98,67 @@ class GooglePubSubBroadcaster extends Broadcaster
 
     protected function formatChannels(array $channels)
     {
-        $collection = new Collection($channels);
-
-        return $collection->map(function (Channel $channel) {
-            if (property_exists($channel, 'topic')) {
-                return (string) $channel->topic;
+        return (new Collection($channels))->map(function ($channel) {
+            if (is_string($channel)) {
+                return $channel;
+            }
+            if ($channel instanceof Channel) {
+                return $this->getTopicFromChannel($channel);
             }
 
             return (string) $channel;
         });
     }
 
-    protected function publish($topic, $event, array $payload = [])
+    protected function getTopicFromChannel(Channel $channel) : string
     {
-        $topic = $this->createTopic($topic);
-        $topic->publish($this->createPayload($event, $payload));
-    }
-
-    protected function createPayload($event, array $payload = []) : Message
-    {
-        return new Message([
-            'data' => json_encode(compact('event', 'payload'))
-        ]);
-    }
-
-
-    protected function createTopic($topic)
-    {
-        $topic = $this->client->topic($topic);
-        if (!$topic->exists()) {
-            $topic->create();
+        if (property_exists($channel, 'topic')) {
+            return (string) $channel->topic;
         }
 
-        return $topic;
+        return (string) $channel;
+    }
+
+    /**
+     * @param string       $topic
+     * @param string|mixed $event
+     * @param array        $payload
+     *
+     * @throws \Jag\Exceptions\GooglePubSub\TopicNotFoundException
+     */
+    protected function publish(string $topic, $event, array $payload = []) : void
+    {
+        try {
+            $pubSubTopic = $this->createTopic($topic);
+            $pubSubTopic->publish($this->createPayload($pubSubTopic, $event, $payload));
+        } catch (NotFoundException $e) {
+            throw new TopicNotFoundException($topic, $e->getMessage(), $e);
+        }
+    }
+
+    /**
+     * @param string $topic
+     *
+     * @return \Google\Cloud\PubSub\Topic
+     */
+    protected function createTopic(string $topic) : Topic
+    {
+        $pubSubTopic = $this->client->topic($topic);
+        if ($this->autoCreateTopic && !$pubSubTopic->exists()) {
+            $pubSubTopic->create();
+        }
+
+        return $pubSubTopic;
+    }
+
+    protected function createPayload(Topic $topic, $event, array $payload = []) : Message
+    {
+        if ($this->payloadClass !== null) {
+            return $this->payloadClass->getMessage($topic, $event, $payload);
+        }
+
+        return new Message([
+            'data' => json_encode(compact('topic', 'event', 'payload')),
+        ]);
     }
 }
